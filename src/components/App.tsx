f21729187186c13fca794a8751267f84ba683294
christinetg <christinee.taing@gmail.com>
2020-02-08 12:41:54 -0500

    plain button react component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Dropdown from './Dropdown/Dropdown';
+import PlainButton from './Buttons/PlainButton/PlainButton';
 import './App.scss';
 
 export default function App() {
   const handleSelect = () => {
     console.log('selected');
   };
+  const handleClick = () => {
+    console.log('selected');
+  };
   return (
     <div className="App">
       <Dropdown id="module" options={['Banner', 'Grid']} handleSelect={handleSelect} />
+      <PlainButton label="Button" handleClick={handleClick} />
     </div>
   );
 }
---
+++ src/components/Buttons/PlainButton/PlainButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './PlainButton.scss';
+
+export interface PlainButtonProps {
+  /** ID for the element */
+  id?: string;
+  /** Button label*/
+  label: string[];
+  /** Handler for click event */
+  handleClick(): void;
+}
+
+const PlainButton = (props: any) => {
+  const { id, label, handleClick } = props;
+  return (
+    <div className="PlainButton">
+      <button id={id} onClick={handleClick}>
+        {label}
+      </button>
+    </div>
+  );
+};
+
+export default PlainButton;
