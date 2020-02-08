987774e2514d8ec652b26f8f316b49e17a4595b4
christinetg <christinee.taing@gmail.com>
2020-02-08 16:02:32 -0500

    folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import App from './components/App';
+import App from './app/App';
 window.onload = function() {
   ReactDom.render(<App />, document.getElementById('root'));
 };
