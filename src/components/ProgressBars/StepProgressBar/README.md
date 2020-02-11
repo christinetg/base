70e272662a6cdc388952257bbcc172f8af09c32a
Christine Taing <christine.taing@MBP-CTAING.local>
2020-02-11 12:14:50 -0500

    🚚 Step Progress Bar

---
+++ src/components/ProgressBars/StepProgressBar/README.md
@@ -0,0 +1,5 @@
+### Step Progress Bar
+
+Step Progress Bar            |  Step Progress Bar progressing | Step Progress Bar complete
+:-------------------------:|:-------------------------:|:-------------------------:
+![Step Progress Bar](./assets/StepProgressBar-noprogress.png)  |  ![Step Progress Bar](./assets/StepProgressBar-progressing.png) | ![Step Progress Bar](./assets/StepProgressBar-complete.png)
--- src/components/ProgressBars/StepProgressBar/StepProgressBar.tsx
+++ src/components/ProgressBars/StepProgressBar/StepProgressBar.tsx
@@ -2,29 +2,54 @@ import React from 'react';
 import './StepProgressBar.scss';
 
 export interface StepProgressBarProps {
-  /** ID for the element */
+  /* ID for the element */
   id?: string;
+  /* Progress bar steps */
+  steps: string[];
+  /* Progress bar color */
+  progressBarColor?: string;
+  /* background color of progress bar */
+  progressBarBgColor?: string;
+  /* Step range width */
+  rangeWidth?: number;
+  /* Last completed step  */
+  lastCompletedStep?: number;
+  /* Font family for step label */
+  fontFamily?: string;
+  /* Font color */
+  color?: string;
 }
 
 const StepProgressBar = (props: StepProgressBarProps) => {
+  const {
+    id,
+    steps,
+    progressBarColor = '#e65245',
+    progressBarBgColor = '#ebeaed',
+    rangeWidth = 8,
+    lastCompletedStep = 0,
+    fontFamily = "'Nunito', sans-serif",
+    color = '#e65245',
+  } = props;
+
+  const progressBarSteps = steps.map((stepLabel: string, i: number) => {
+    return (
+      <li key={`${stepLabel}-${i}`} className={i + 1 <= lastCompletedStep ? 'completed' : null}>
+        <span>{stepLabel}</span>
+      </li>
+    );
+  });
+
+  document.documentElement.style.setProperty('--stepProgressBar-rangeWidth', `${rangeWidth}rem`);
+  document.documentElement.style.setProperty('--stepProgressBar-bgColor', progressBarBgColor);
+  document.documentElement.style.setProperty('--stepProgressBar-color', progressBarColor);
+  document.documentElement.style.setProperty('--stepProgressBar-fontColor', color);
+
   return (
     <div className="StepProgressBar">
-      {/* <ul className="step-progress-bar"> */}
-      <div className="step-progress-bar">
-        <li className="active">
-          <span>Step 1</span>
-        </li>
-        <li className="active">
-          <span>Step 2</span>
-        </li>
-        <li className="active">
-          <span>Step 3</span>
-        </li>
-        <li>
-          <span>Step 4</span>
-        </li>
+      <div id={id} className="step-progress-bar" style={{ fontFamily }}>
+        {progressBarSteps}
       </div>
-      {/* </ul> */}
     </div>
   );
 };
