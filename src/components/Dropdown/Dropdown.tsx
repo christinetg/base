b9523a5ee7d61247d9e02746d5d39e0aab9617af
christinetg <christinee.taing@gmail.com>
2020-02-11 22:21:55 -0500

    🧹 Clean up unused import

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiChevronUp, FiChevronDown } from 'react-icons/fi';
 import './Dropdown.scss';
 
